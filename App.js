5e587382b88f2d87554dfcee94a16957ed4f45ad
krystianwojtowicz <60481782+krystianwojtowicz@users.noreply.github.com>
2020-05-17 10:11:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 class App extends React.Component {
 
   state = {
-    omen: ['pierwsza wróżba', 'druga wróżba', 'trzecia wróżba'],
-    text: null
+    value: "",
+    omens: ['pierwsza wróżba', 'druga wróżba', 'trzecia wróżba'],
+    text: null,
   }
 
-  handleClicked = () => {
-    const number = Math.floor(Math.random() * this.state.omen.length)
+  handleSelectOmen = () => {
+    const number = Math.floor(Math.random() * this.state.omens.length)
     this.setState({
-      text: this.state.omen[number]
+      text: this.state.omens[number]
+    })
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  handleAddOmen = () => {
+    const omens = this.state.omens.concat(this.state.value);
+    this.setState({
+      omens,
+      value: "",
     })
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClicked}>losuj wróżbę</button>
+        <button onClick={this.handleSelectOmen}>losuj wróżbę</button>
+        <br />
+        <input type="text" value={this.state.value} onChange={this.handleInputChange}></input>
+        <button onClick={this.handleAddOmen}>dodaj wróżbę</button>
         {this.state.text ? <h1>{this.state.text}</h1> : null}
       </div>
     );
